9e97bdf66176c9ae754e3ae203ffc8fbf0a47f5a
nams238 <onnamdiu@gmail.com>
2022-05-05 14:22:22 +0100

    question bank added on js

---
+++ assets/index.js
@@ -0,0 +1,28 @@
+// Question Bank
+var questionBank= [
+    {
+        question : 'Grand Central Terminal, Park Avenue, New York is the world',
+        option : ['largest railway station','highest railway station','longest railway station','None of the above'],
+        answer : 'largest railway station'
+    },
+    {
+        question : 'Who is the Prime Minister of United Kingdom',
+        option : ['Tony Brown','Jimmy Smith','Michael Jordan','Boris Johnson'],
+        answer :  'Boris Johnson'  
+    },
+    {
+        question : 'What is the capital of Nigeria',
+        option : ['London','Abuja','Abijan','Tokyo'],
+        answer : 'Abuja'
+    },
+    {
+        question : 'Who is the President of France',
+        option : ['Boris Johnson','Danjuma Ahmad','Emmanuel Macron','Marine Le Pen'],
+        answer : 'Emmanuel Macron'
+    },
+    {
+        question : 'What is the capital of England',
+        option : ['Aberdeen','Glasgow','London','Paris'],
+        answer : 'London'
+    }
+]
