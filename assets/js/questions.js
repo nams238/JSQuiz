063801bb6f87f7d8625396d57e2c016ec0c74096
nams238 <onnamdiu@gmail.com>
2022-05-05 20:53:35 +0100

    asset folder created, css, js linked, questions added

---
+++ assets/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Javascript is a..... -side programming language?",
+    answer: "Both",
+    options: [
+      "Client",
+      "Server",
+      "None",
+      "Both"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What year was JavaScript created?",
+    answer: "1995",
+    options: [
+      "1995",
+      "2022",
+      "1996",
+      "2001"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Inside which HTML element do we put the JavaScript?",
+    answer: "Script",
+    options: [
+      "Scripting",
+      "Javascript",
+      "Script",
+      "None of the above",
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which JavaScript label catches all the values, except for the ones specified?",
+    answer: "default",
+    options: [
+      "try",
+      "label",
+      "default",
+      "catch"
+    ]
+  },
+    {
+    numb: 5,
+    question: "JavaScript File Has An Extension of?",
+    answer: "Js",
+    options: [
+      ".java",
+      ".js",
+      "script",
+      "Scripting"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
